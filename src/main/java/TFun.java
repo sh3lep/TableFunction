9af229a52d0c31c52a05dd0328a7f2127db992c2
sh3lep <sh3lep@gmail.com>
2018-03-18 15:58:02 +0300

    Class fix, tests started

--- src/main/java/TFun.java
+++ src/main/java/TFun.java
@@ -3,63 +3,85 @@ import javafx.util.Pair;
 
 import java.util.*;
 
-public class TFun {
-    public Map<Double, Double> table;
+import static java.lang.Math.abs;
 
-    TFun(Map<Double, Double> table) {
-        this.table = table;
+public class TFun {
+    public ArrayList<Double> X;
+    public ArrayList<Double> Y;
+
+    TFun(){
+        X = new ArrayList<>();
+        Y = new ArrayList<>();
+    }
+
+    TFun(ArrayList<Double> X, ArrayList<Double> Y) {
+        this.X = X;
+        this.Y = Y;
     }
 
     private Boolean hasPair(Double x, Double y) {
-        return table.containsKey(x) && table.containsValue(y);
+        return X.contains(x) && Y.contains(y);
     }
 
     public void addPair(Double x, Double y) {
         if (!hasPair(x, y)) {
-            table.put(x, y);
+            X.add(x);
+            Y.add(y);
         } else throw new IllegalArgumentException("Пара уже существует");
     }
 
     public void deletePair(Double x, Double y) {
         if (hasPair(x, y)) {
-            table.remove(x, y);
+            X.remove(x);
+            Y.remove(y);
         } else throw new IllegalArgumentException("Пара не найдена");
     }
 
-    public Map<Double, Double> getTable() {
-        return table;
+    public ArrayList<Pair<Double,Double>> getTable() {
+        ArrayList<Pair<Double,Double>> list = new ArrayList<>();
+        for (int i =0; i < X.size();i++){
+            list.add(new Pair<>(X.get(i),Y.get(i)));
+        }
+        return list;
     }
 
     public Pair<Double, Double> getClosest(Double x) {
         Double closestX = Double.MAX_VALUE;
-        for (Double current : table.keySet()) {
-            if (current - x < closestX - x) {
+        for (Double current : X) {
+            if (abs(current - x) < abs(closestX - x)) {
                 closestX = current;
             }
 
         }
-        return new Pair<>(closestX, table.get(closestX));
+        return new Pair<>(closestX,Y.get(X.indexOf(closestX)));
     }
 
     public Double getApproximate(Double x) {
-        if (hasPair(x, table.get(x))) {
+        if (hasPair(x, Y.get(X.indexOf(x)))) {
             throw new IllegalArgumentException("Существует точное значение");
         }
         Double leftX = Double.MAX_VALUE;
         Double rightX = Double.MAX_VALUE;
-        for (Double current : table.keySet()) {
+        for (Double current : X) {
             if (current - x < leftX - x && current < x) {
                 leftX = current;
             }
         }
-        for (Double current : table.keySet()) {
+        for (Double current : X) {
             if (current - x < rightX - x && current > x) {
                 rightX = current;
             }
         }
-        return table.get(leftX) + (x - leftX) / (rightX - leftX) * (table.get(rightX) - table.get(leftX));
+        return X.get(X.indexOf(leftX)) + (x - leftX) / (rightX - leftX) * (X.get(X.indexOf(rightX)) - X.get(X.indexOf(leftX)));
     }
 
+    @Override
+    public String toString() {
+        return "TFun{" +
+                "X=" + X +
+                ", Y=" + Y +
+                '}';
+    }
 
     @Override
     public boolean equals(Object o) {
@@ -68,12 +90,14 @@ public class TFun {
 
         TFun tFun = (TFun) o;
 
-        return table != null ? table.equals(tFun.table) : tFun.table == null;
+        if (X != null ? !X.equals(tFun.X) : tFun.X != null) return false;
+        return Y != null ? Y.equals(tFun.Y) : tFun.Y == null;
     }
 
     @Override
     public int hashCode() {
-        return table != null ? table.hashCode() : 0;
+        int result = X != null ? X.hashCode() : 0;
+        result = 31 * result + (Y != null ? Y.hashCode() : 0);
+        return result;
     }
-
 }
--- src/test/java/TFunTest.java
+++ src/test/java/TFunTest.java
@@ -1,8 +1,24 @@
+import static org.junit.Assert.*;
 
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+
 
 class TFunTest {
-    public void sefoojpse (){
-        TFun
+
+
+    @Test
+    public void addPair() {
+        TFun table = new TFun();
+        table.addPair(2.28, 13.37);
+        assertEquals(2.28, table.X.get(0));
     }
+
+    @Test
+    public void deletePair() {
+        TFun table = new TFun();
+        assertTrue(table.deletePair(2.28, 13.37));
+    }
+
 }
